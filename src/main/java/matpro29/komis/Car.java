0ffa4f9aa699e09fcd77c15efb3f2043453944a7
Mateusz Proskura <matpro29@outlook.com>
2019-03-16 23:33:52 +0100

    Obsluga klasy Car

---
+++ src/main/java/matpro29/komis/Car.java
@@ -0,0 +1,33 @@
+package matpro29.komis;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/matpro29/komis/KomisApplication.java
+++ src/main/java/matpro29/komis/KomisApplication.java
@@ -1,7 +1,11 @@
 package matpro29.komis;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class KomisApplication {
@@ -10,4 +14,16 @@ public class KomisApplication {
         SpringApplication.run(KomisApplication.class, args);
     }
 
+    @Bean
+    ApplicationRunner init(CarRepository repository) {
+        return args -> {
+            Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+                    "AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
+                Car car = new Car();
+                car.setName(name);
+                repository.save(car);
+            });
+            repository.findAll().forEach(System.out::println);
+        };
+    }
 }
