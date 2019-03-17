f2d69d5d0ca7ec17da7e58c0058d97fa879efd6b
Mateusz Proskura <matpro29@outlook.com>
2019-03-17 17:23:41 +0100

    Podpiecie bazy mySQL

--- src/main/java/matpro29/komis/KomisApplication.java
+++ src/main/java/matpro29/komis/KomisApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -14,16 +13,16 @@ public class KomisApplication {
         SpringApplication.run(KomisApplication.class, args);
     }
 
-    @Bean
-    ApplicationRunner init(CarRepository repository) {
-        return args -> {
-            Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
-                    "AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
-                Car car = new Car();
-                car.setName(name);
-                repository.save(car);
-            });
-            repository.findAll().forEach(System.out::println);
-        };
-    }
+//    @Bean
+//    ApplicationRunner init(CarRepository repository) {
+//        return args -> {
+//            Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+//                    "AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
+//                Car car = new Car();
+//                car.setName(name);
+//                repository.save(car);
+//            });
+//            repository.findAll().forEach(System.out::println);
+//        };
+//    }
 }
