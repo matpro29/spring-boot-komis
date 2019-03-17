0de123b9edcfdd8870e3c1eb518c04ef8a552c41
Mateusz Proskura <matpro29@outlook.com>
2019-03-17 13:41:25 +0100

    Lista samochodow

---
+++ src/main/java/matpro29/komis/CarController.java
@@ -0,0 +1,31 @@
+package matpro29.komis;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RestController
+public class CarController {
+    private CarRepository repository;
+
+    public CarController(CarRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/cars")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public Collection<Car> ars() {
+        return repository.findAll().stream()
+                .filter(this::isCool)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isCool(Car car) {
+        return !car.getName().equals("AMC Gremlin") &&
+                !car.getName().equals("Triumph Stag") &&
+                !car.getName().equals("Ford Pinto") &&
+                !car.getName().equals("Yugo GV");
+    }
+}
